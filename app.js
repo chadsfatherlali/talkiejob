e1c1f78c808784234982a9ab12b530d3054cabe1
Santiago Sánchez <chadsfather@gmail.com>
2016-12-19 18:45:48 +0100

    Commit inicial

---
+++ app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const messages = require('./controllers/messages');
+const home = require('./controllers/home');
+const compress = require('koa-compress');
+const logger = require('koa-logger');
+const serve = require('koa-static');
+const route = require('koa-route');
+const koa = require('koa');
+const path = require('path');
+const app = module.exports = koa();
+
+// Logger
+app.use(logger());
+
+/*app.use(route.get('/', messages.home));
+app.use(route.get('/messages', messages.list));
+app.use(route.get('/messages/:id', messages.fetch));
+app.use(route.post('/messages', messages.create));
+app.use(route.get('/async', messages.delay));
+app.use(route.get('/promise', messages.promise));*/
+
+app.use(route.get('/', home.home));
+
+// Serve static files
+app.use(serve(path.join(__dirname, 'public')));
+
+// Compress
+app.use(compress());
+
+if (!module.parent) {
+  app.listen(3000);
+  console.log('listening on port 3000');
+}
